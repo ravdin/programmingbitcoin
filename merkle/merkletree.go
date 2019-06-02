326d44b657c9d6f8b3a24637a5a302fa35081638
Bill Ravdin <bill.ravdin@gmail.com>
2019-06-02 16:07:59 -0700

    Added MerkleTree

---
+++ merkle/merkletree.go
@@ -0,0 +1,139 @@
+package merkle
+
+import (
+	"errors"
+	"fmt"
+	"math"
+	"strings"
+
+	"github.com/ravdin/programmingbitcoin/util"
+)
+
+type MerkleTree struct {
+	Total        int
+	MaxDepth     int
+	Nodes        [][][]byte
+	CurrentDepth int
+	CurrentIndex int
+}
+
+func NewMerkleTree(total int) *MerkleTree {
+	result := &MerkleTree{Total: total}
+	result.MaxDepth = int(math.Ceil(math.Log2(float64(total))))
+	result.Nodes = make([][][]byte, result.MaxDepth+1)
+	denom := math.Pow(2.0, float64(result.MaxDepth))
+	for i, _ := range result.Nodes {
+		numItems := int(math.Ceil(float64(total) / denom))
+		result.Nodes[i] = make([][]byte, numItems)
+		denom /= 2
+	}
+	result.CurrentDepth = 0
+	result.CurrentIndex = 0
+	return result
+}
+
+func (self *MerkleTree) String() string {
+	result := make([]string, len(self.Nodes))
+	for depth, level := range self.Nodes {
+		items := make([]string, len(level))
+		for index, h := range level {
+			if len(h) == 0 {
+				items[index] = "None"
+			} else if depth == self.CurrentDepth && index == self.CurrentIndex {
+				items[index] = fmt.Sprintf("*%x.*", h[:4])
+			} else {
+				items[index] = fmt.Sprintf("%x...", h[:4])
+			}
+		}
+		result[depth] = strings.Join(items, ", ")
+	}
+	return strings.Join(result, "\n")
+}
+
+func (self *MerkleTree) PopulateTree(flagBits []byte, hashes [][]byte) error {
+	for len(self.root()) == 0 {
+		if self.isLeaf() {
+			flagBits = flagBits[1:]
+			self.setCurrentNode(hashes[0])
+			hashes = hashes[1:]
+			self.up()
+			continue
+		}
+		leftHash := self.getLeftNode()
+		if len(leftHash) == 0 {
+			flagBit := flagBits[0]
+			flagBits = flagBits[1:]
+			if flagBit == 0 {
+				self.setCurrentNode(hashes[0])
+				hashes = hashes[1:]
+				self.up()
+			} else {
+				self.left()
+			}
+		} else if self.rightExists() {
+			rightHash := self.getRightNode()
+			if len(rightHash) == 0 {
+				self.right()
+			} else {
+				self.setCurrentNode(util.MerkleParent(leftHash, rightHash))
+				self.up()
+			}
+		} else {
+			self.setCurrentNode(util.MerkleParent(leftHash, leftHash))
+			self.up()
+		}
+	}
+	if len(hashes) != 0 {
+		return errors.New(fmt.Sprintf("hashes not all consumed %d", len(hashes)))
+	}
+	for _, flagBit := range flagBits {
+		if flagBit != 0 {
+			return errors.New("flagBits not all consumed")
+		}
+	}
+	return nil
+}
+
+func (self *MerkleTree) up() {
+	self.CurrentDepth--
+	self.CurrentIndex >>= 1
+}
+
+func (self *MerkleTree) left() {
+	self.CurrentDepth++
+	self.CurrentIndex <<= 1
+}
+
+func (self *MerkleTree) right() {
+	self.CurrentDepth++
+	self.CurrentIndex <<= 1
+	self.CurrentIndex++
+}
+
+func (self *MerkleTree) root() []byte {
+	return self.Nodes[0][0]
+}
+
+func (self *MerkleTree) setCurrentNode(value []byte) {
+	self.Nodes[self.CurrentDepth][self.CurrentIndex] = value
+}
+
+func (self *MerkleTree) getCurrentNode() []byte {
+	return self.Nodes[self.CurrentDepth][self.CurrentIndex]
+}
+
+func (self *MerkleTree) getLeftNode() []byte {
+	return self.Nodes[self.CurrentDepth+1][self.CurrentIndex*2]
+}
+
+func (self *MerkleTree) getRightNode() []byte {
+	return self.Nodes[self.CurrentDepth+1][self.CurrentIndex*2+1]
+}
+
+func (self *MerkleTree) isLeaf() bool {
+	return self.CurrentDepth == self.MaxDepth
+}
+
+func (self *MerkleTree) rightExists() bool {
+	return len(self.Nodes[self.CurrentDepth+1]) > self.CurrentIndex*2+1
+}
---
+++ merkle/merkletree_test.go
@@ -0,0 +1,75 @@
+package merkle
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/ravdin/programmingbitcoin/util"
+)
+
+func TestMerkleTree(t *testing.T) {
+	t.Run("test init", func(t *testing.T) {
+		tree := NewMerkleTree(9)
+		values := []int{1, 2, 3, 5, 9}
+		for i, value := range values {
+			actual := len(tree.Nodes[i])
+			expected := value
+			if actual != expected {
+				t.Errorf("Expected %d, got %d", expected, actual)
+			}
+		}
+	})
+
+	t.Run("test populate tree 1", func(t *testing.T) {
+		hashes := [][]byte{
+			util.HexStringToBytes("9745f7173ef14ee4155722d1cbf13304339fd00d900b759c6f9d58579b5765fb"),
+			util.HexStringToBytes("5573c8ede34936c29cdfdfe743f7f5fdfbd4f54ba0705259e62f39917065cb9b"),
+			util.HexStringToBytes("82a02ecbb6623b4274dfcab82b336dc017a27136e08521091e443e62582e8f05"),
+			util.HexStringToBytes("507ccae5ed9b340363a0e6d765af148be9cb1c8766ccc922f83e4ae681658308"),
+			util.HexStringToBytes("a7a4aec28e7162e1e9ef33dfa30f0bc0526e6cf4b11a576f6c5de58593898330"),
+			util.HexStringToBytes("bb6267664bd833fd9fc82582853ab144fece26b7a8a5bf328f8a059445b59add"),
+			util.HexStringToBytes("ea6d7ac1ee77fbacee58fc717b990c4fcccf1b19af43103c090f601677fd8836"),
+			util.HexStringToBytes("457743861de496c429912558a106b810b0507975a49773228aa788df40730d41"),
+			util.HexStringToBytes("7688029288efc9e9a0011c960a6ed9e5466581abf3e3a6c26ee317461add619a"),
+			util.HexStringToBytes("b1ae7f15836cb2286cdd4e2c37bf9bb7da0a2846d06867a429f654b2e7f383c9"),
+			util.HexStringToBytes("9b74f89fa3f93e71ff2c241f32945d877281a6a50a6bf94adac002980aafe5ab"),
+			util.HexStringToBytes("b3a92b5b255019bdaf754875633c2de9fec2ab03e6b8ce669d07cb5b18804638"),
+			util.HexStringToBytes("b5c0b915312b9bdaedd2b86aa2d0f8feffc73a2d37668fd9010179261e25e263"),
+			util.HexStringToBytes("c9d52c5cb1e557b92c84c52e7c4bfbce859408bedffc8a5560fd6e35e10b8800"),
+			util.HexStringToBytes("c555bc5fc3bc096df0a0c9532f07640bfb76bfe4fc1ace214b8b228a1297a4c2"),
+			util.HexStringToBytes("f9dbfafc3af3400954975da24eb325e326960a25b87fffe23eef3e7ed2fb610e"),
+		}
+		flagBits := make([]byte, 31)
+		for i, _ := range flagBits {
+			flagBits[i] = 1
+		}
+		tree := NewMerkleTree(len(hashes))
+		tree.PopulateTree(flagBits, hashes)
+		expected := `597c4bafe3832b17cbbabe56f878f4fc2ad0f6a402cee7fa851a9cb205f87ed1`
+		actual := fmt.Sprintf("%x", tree.root())
+		if actual != expected {
+			t.Errorf("Expected %s, got %s", expected, actual)
+		}
+	})
+
+	t.Run("test populate tree 2", func(t *testing.T) {
+		hashes := [][]byte{
+			util.HexStringToBytes("42f6f52f17620653dcc909e58bb352e0bd4bd1381e2955d19c00959a22122b2e"),
+			util.HexStringToBytes("94c3af34b9667bf787e1c6a0a009201589755d01d02fe2877cc69b929d2418d4"),
+			util.HexStringToBytes("959428d7c48113cb9149d0566bde3d46e98cf028053c522b8fa8f735241aa953"),
+			util.HexStringToBytes("a9f27b99d5d108dede755710d4a1ffa2c74af70b4ca71726fa57d68454e609a2"),
+			util.HexStringToBytes("62af110031e29de1efcad103b3ad4bec7bdcf6cb9c9f4afdd586981795516577"),
+		}
+		flagBits := make([]byte, 11)
+		for i, _ := range flagBits {
+			flagBits[i] = 1
+		}
+		tree := NewMerkleTree(len(hashes))
+		tree.PopulateTree(flagBits, hashes)
+		expected := `a8e8bd023169b81bc56854137a135b97ef47a6a7237f4c6e037baed16285a5ab`
+		actual := fmt.Sprintf("%x", tree.root())
+		if actual != expected {
+			t.Errorf("Expected %s, got %s", expected, actual)
+		}
+	})
+}
